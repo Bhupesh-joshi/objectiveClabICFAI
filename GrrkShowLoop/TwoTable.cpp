262ab071363fde75300723173c52ea7f189aa01d
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2023-10-04 23:24:38 +0530

    woking on patterns

---
+++ GrrkShowLoop/TwoTable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    int a = 0;
+    int i;
+
+for (i = 0; i <10; i++) {
+    a = a+2;
+    cout << a << endl;
+}
+
+}
---
+++ GrrkShowLoop/forLoopOne.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    int i;
+
+//     for (initilization; condition; increase/decrese){
+//         statement
+//     }
+
+for (i = 0; i < -10; i--) {
+    cout << "I is " << i << endl;
+}
+
+
+}
