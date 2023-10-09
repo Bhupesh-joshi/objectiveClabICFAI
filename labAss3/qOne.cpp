83217d277b97501a6bb8a514b3dc61dcc9e9d1db
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2023-10-10 01:02:56 +0530

    Ass-3 qOne done modified

--- labAss3/qOne.cpp
+++ labAss3/qOne.cpp
@@ -1,1 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+
+    // Input three numbers
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    // Find the maximum among the three numbers
+    int largest = max(num1, max(num2, num3));
+
+    cout << "The largest number is: " << largest << endl;
+
+    return 0;
+}
