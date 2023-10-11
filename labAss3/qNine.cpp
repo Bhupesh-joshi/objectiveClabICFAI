949223b925620c0aed9d9b9c61d3f0e8f3896689
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2023-10-11 13:00:52 +0530

    qnine getting error ASS-3

--- labAss3/qNine.cpp
+++ labAss3/qNine.cpp
@@ -1,1 +1,28 @@
-#include <string>
+#include <iostream>
+
+int main() {
+    int a, b, c, d, x;
+
+    // Read 5 distinct numbers
+    std::cout << "Enter five distinct numbers (a, b, c, d): ";
+    std::cin >> a >> b >> c >> d;
+
+    // Read the extra number
+    std::cout << "Enter an extra number (x): ";
+    std::cin >> x;
+
+    // Check which number matches x and output it
+    if (x == a) {
+        std::cout << "x is equal to a: " << a << std::endl;
+    } else if (x == b) {
+        std::cout << "x is equal to b: " << b << std::endl;
+    } else if (x == c) {
+        std::cout << "x is equal to c: " << c << std::endl;
+    } else if (x == d) {
+        std::cout << "x is equal to d: " << d << std::endl;
+    } else {
+        std::cout << "x is not equal to any of the numbers a, b, c, or d." << std::endl;
+    }
+
+    return 0;
+}
